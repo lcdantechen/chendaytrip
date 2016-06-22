ae3255dbef3041cb0aee6764e350c2b2ab1baf1f
Dante <dante@DantetekiAir.home>
2016-06-22 13:17:31 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,80 @@ $( document ).ready(function() {
               
             getLocation();
 			
+//-on lick funciton that converts city name to location ------------------------------------------------------------------------
+
+$("#citySearch").on("click", function() {
+
+					var name = $('#cityName').val();
+				   var geocoder =  new google.maps.Geocoder();
+           geocoder.geocode( { 'address': name}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            alert("location : " + results[0].geometry.location.lat() + " " +results[0].geometry.location.lng()); 
+          } else {
+            alert("Something got wrong " + status);
+          }
+
+			          var latitude = results[0].geometry.location.lat();
+								var longitude = results[0].geometry.location.lng();
+                 console.log("Latitude : " + latitude + " Longitude: " + longitude);
+						            //eventbrite use latitude and longitude
+						                var eventBriteUrl = "https://www.eventbriteapi.com/v3/";
+						                var token = "OO4THRQ4RMB522E4DLLG";
+						                $.ajax({
+						                  type: "GET",
+						                  url: eventBriteUrl + "events/search/?location.within=100km&location.latitude="+latitude+"&location.longitude="+longitude+"&token="+token,
+						      //------dataType solve console errer: Node cannot be inserted at the specified point in the hierarchy
+						                 dataType:'json',
+						                  success: function(response){
+						                  	console.log('success')
+						                   console.log('response------------------------------', response);
+						      //-----------for loop the event result out------
+						                    var result = response.events;
+                            for (var i=0; i<20; i++){
+                              console.log(result[i]);
+                              var eventDiv = $('<div>');
+                              eventDiv.addClass('card');
+                              var eventIntro = $('<p>');
+                              eventIntro.addClass('card-block');
+
+                              var introTitle = $('<h4>');
+                              introTitle.addClass('card-title');
+                              introTitle.text(result[i].name.text);
+                              
+                              var startTime = result[i].start.local;
+                              var introText = $('<p>');
+                              introText.addClass('card-text');
+                              introText.text(moment(startTime).format('dddd MMMM Do, hh:mm a'));
+
+
+                              var eventImg = $('<img>');
+                              eventImg.addClass('card-img-top');
+                              eventImg.attr('src', result[i].logo.url)
+
+                              eventIntro.append(introTitle);
+                              eventIntro.append(introText);
+                              eventDiv.append(eventImg);
+                              eventDiv.append(eventIntro);
+
+                              var eventDiv2 = $('<div>');
+                              eventDiv2.append(eventDiv);
+                              $('#events').html(eventDiv2);
+                              }
+						      // -------Loop ends ----------
+
+						                 }
+						                  
+						              });
+						   
+
+
+
+        });
+						   
+});
+
+
+//---------------------------------------------------------------------------
 						//geolocation functions 
 						function showLocation(position) {
 						            var latitude = position.coords.latitude;
@@ -77,12 +151,12 @@ $( document ).ready(function() {
                               eventDiv.append(eventImg);
                               eventDiv.append(eventIntro);
                               $('#events').append(eventDiv);
-                            }
+                              }
 						      // -------Loop ends ----------
 
-						                  }
+						                 }
 						                  
-						                });
+						              });
 						   
 						         }
 
@@ -114,5 +188,3 @@ $( document ).ready(function() {
 
 
 });
-
-
