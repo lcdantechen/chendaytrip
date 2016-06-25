d9458e80163d99cde8668cbb0679f6e408ceae51
Dante <dante@DantetekiAir.fios-router.home>
2016-06-25 01:58:20 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -144,7 +144,7 @@ $("#citySearch").on("click", function() {
           showUserLocation();
 
 //eventbrite use latitude and longitude
-				  eventsBrite()
+				  eventsBrite();
 						   
 
 
@@ -193,23 +193,8 @@ $("#citySearch").on("click", function() {
               }
 
 
-           /*   function getUserLocation(){
 
-						            if(navigator.geolocation){
-						               // timeout at 60000 milliseconds (60 seconds)
-						               var options = {timeout:60000};
-						               navigator.geolocation.getCurrentPosition(showUserLocation, errorHandler, options);
-						            }
-						            
-						            else{
-						               alert("Sorry, browser does not support geolocation!");
-						            }
-						         }*/
-
-//---------------------------------------------------------------------------
-
-
-//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------//
 						//geolocation functions 
 						function showLocation(position) {
 							console.log('in show location');
@@ -238,13 +223,71 @@ $("#citySearch").on("click", function() {
 											                      });
 											    cityCircle.setMap(map);
 
-
-
-
                   //----------gogole map ends-----------
 						            console.log("Latitude : " + latitude + " Longitude: " + longitude);
 						            //eventbrite use latitude and longitude
-						            eventsBrite();
+						               var eventBriteUrl = "https://www.eventbriteapi.com/v3/";
+										                var token = "OO4THRQ4RMB522E4DLLG";
+										                $.ajax({
+										                  type: "GET",
+										                  url: eventBriteUrl + "events/search/?expand=venue&location.within=50km&location.latitude="+latitude+"&location.longitude="+longitude+"&token="+token,
+				//------dataType solve console errer: Node cannot be inserted at the specified point in the hierarchy
+										                 dataType:'json',
+										                  success: function(response){
+										                  	$('#events').empty();
+										                  	console.log('success')
+										                   console.log('response------------------------------', response);
+				//-----------for loop the event result out------
+										                  var result = response.events;
+				                              for (var i=0; i<40; i++){
+				                              console.log(result[i]);
+				                              var eventDiv = $('<div>');
+				                              eventDiv.addClass('card');
+				                              var eventIntro = $('<p>');
+				                              eventIntro.addClass('card-block');
+
+				                              var introTitle = $('<h4>');
+				                              introTitle.addClass('card-title');
+				                              introTitle.text(result[i].name.text);
+				                              
+				                              
+																			/*var addText = $('<p>');
+																			addText.addClass('card-text');
+																			addText.html(result[i].venue.address.address_1 + "<br/>" + result[i].venue.address.city );
+*/
+				                              var startTime = result[i].start.local;
+				                              var introText = $('<p>');
+				                              introText.addClass('card-text');
+				                              introText.text(moment(startTime).format('dddd MMMM Do, hh:mm a'));
+
+
+				                              var eventImg = $('<img>');
+				                              eventImg.addClass('card-img-top');
+				                              eventImg.attr('src', result[i].logo.url)
+
+				                              var eventurl = $('<a/>');
+																			eventurl.addClass('card-link');
+																			eventurl.text(result[i].vanity_url);
+																			eventurl.attr('href', result[i].url);
+
+				                              eventIntro.append(introTitle);
+				                              eventIntro.append(introText);
+				                              /*eventIntro.append(addText);*/
+					                            eventIntro.append(eventurl);
+				                              eventDiv.append(eventImg);
+				                              eventDiv.append(eventIntro);
+
+				                              
+				                              console.log('response------------------------------------------');
+				                             
+				                              $('#events').prepend(eventDiv);
+				                              }
+				                             
+										      // -------Loop ends ----------
+
+										                 }
+										                  
+										              });
 						   
 						         };
 
@@ -276,7 +319,7 @@ $("#citySearch").on("click", function() {
 										                var token = "OO4THRQ4RMB522E4DLLG";
 										                $.ajax({
 										                  type: "GET",
-										                  url: eventBriteUrl + "events/search/?location.within=100km&location.latitude="+latitude+"&location.longitude="+longitude+"&token="+token,
+										                  url: eventBriteUrl + "events/search/?location.within="+ travelRadius+ "km&location.latitude="+latitude+"&location.longitude="+longitude+"&token="+token,
 				//------dataType solve console errer: Node cannot be inserted at the specified point in the hierarchy
 										                 dataType:'json',
 										                  success: function(response){
